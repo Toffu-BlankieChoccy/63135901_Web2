d868bf709765454cccdb31a7c33ed86a9a538ba2
Toffu-kum <trnhoangtrong@gmail.com>
2024-03-05 17:19:08 +0700

    Springboot commit

---
+++ SpringMVCBai1Maven/src/main/webapp/WEB-INF/views/IHateMyselfSrpingBoot/src/test/java/com/example/IHateMyselfSrpingBoot/IHateMyselfSrpingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IHateMyselfSrpingBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHateMyselfSrpingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
