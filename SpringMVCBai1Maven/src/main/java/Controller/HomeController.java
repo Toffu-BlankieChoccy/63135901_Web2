6b654dce67f06eab2139b33a6d09d5317f531ce5
Toffu-kum <trnhoangtrong@gmail.com>
2024-03-05 08:58:04 +0700

    upload chocolate pie yummy mmmhmmm

--- SpringMVCBai1Maven/src/main/java/Controller/HomeController.java
+++ SpringMVCBai1Maven/src/main/java/Controller/HomeController.java
@@ -1,7 +1,10 @@
+package Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
-public class WelcomeController {
+public class HomeController {
 	@RequestMapping("/home")
 	public String home() {
 		return "home";
