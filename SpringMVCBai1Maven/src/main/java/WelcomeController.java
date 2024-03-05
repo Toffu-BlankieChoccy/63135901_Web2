af2ec4e10ff6c42f04a2f90a34984054b9ae3669
Toffu-kum <trnhoangtrong@gmail.com>
2024-03-05 08:51:21 +0700

    upload chocolate pie yummy mmmhmmm

---
+++ SpringMVCBai1Maven/src/main/java/WelcomeController.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class WelcomeController {
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
