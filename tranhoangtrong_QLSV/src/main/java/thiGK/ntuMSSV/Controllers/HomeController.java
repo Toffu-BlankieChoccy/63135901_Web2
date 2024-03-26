f21d0fdb2fbb4b46e6539c90731b31513ae3bfc2
Toffu-kum <trnhoangtrong@gmail.com>
2024-03-26 16:44:45 +0700

    New commit

--- tranhoangtrong_QLSV/src/main/java/thiGK/ntuMSSV/Controllers/HomeController.java
+++ tranhoangtrong_QLSV/src/main/java/thiGK/ntuMSSV/Controllers/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import thiGK.ntuMSSV.Services.SinhVienService;
 
@@ -13,13 +15,33 @@ import thiGK.ntuMSSV.Models.SinhVien;
 
 @Controller
 public class HomeController {
-	@Autowired
-	private SinhVienService sinhVienService;
-	
 	@GetMapping("/")
-	public String home(Model model) {
-		List<SinhVien> danhSachSinhVien = sinhVienService.dsSinhViens();
-        model.addAttribute("danhSachSinhVien", danhSachSinhVien);
+	public String home(Model model) {		
 		return "index";
 	}
+	
+	@Autowired 
+	private SinhVienService sinhVienService;
+	@GetMapping("/sinhvien")
+	public String sinhVien(Model model) {
+		List<SinhVien> danhSachSinhVien = sinhVienService.dsSinhViens();
+        model.addAttribute("danhSachSinhVien", danhSachSinhVien);        
+		return "sinhvien";
+	}
+	
+	@GetMapping("/add")
+	public String add(Model model) {
+		SinhVien sinhVien = new SinhVien();
+        model.addAttribute("sinhVien", sinhVien);
+		return "add";
+	}
+	
+	@RequestMapping("/addSV")
+    public String addSV(Model model, @ModelAttribute("sinhVien") SinhVien sinhVien) {
+        SinhVienService.themMoiSinhVien(sinhVien);
+        return "redirect:/";
+    }
 }
+
+
+
