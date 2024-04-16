6227650231ab588dc13a781d0fdbe18d60f5413f
Toffu-kum <trnhoangtrong@gmail.com>
2024-04-16 15:19:37 +0700

    Initial commit for Project

---
+++ business_web_app_project/src/main/java/tech/toffu/business_web_app_project/BusinessWebAppProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.toffu.business_web_app_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessWebAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessWebAppProjectApplication.class, args);
+	}
+
+}
---
+++ business_web_app_project/src/test/java/tech/toffu/business_web_app_project/BusinessWebAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.toffu.business_web_app_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessWebAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
