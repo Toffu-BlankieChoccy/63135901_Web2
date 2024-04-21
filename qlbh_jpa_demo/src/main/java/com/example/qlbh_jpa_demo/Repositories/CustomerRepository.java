3d71b280fa6220cc634af5066eb5309a9083e710
Toffu-kum <trnhoangtrong@gmail.com>
2024-04-21 19:12:04 +0700

    Database and model update

--- qlbh_jpa_demo/src/main/java/com/example/qlbh_jpa_demo/Repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlbh_jpa_demo.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.qlbh_jpa_demo.Models.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
